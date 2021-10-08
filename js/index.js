6e3f9a180c02a2f1587732654c7712558f3f5106
unknown <prema.chhaya@gmail.com>
2021-10-08 13:52:24 +0530

    trying to lauch portfolio again

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle'); 
+const navLinks = document.querySelector('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link =>
+{ 
+    navLinks.addEventListener('click', () => 
+    {
+        document.body.classList.remove('nav-open');
+})
+})
